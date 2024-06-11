e6d14ab1063adff2b8355a0c11228c15c92aaf6f
tianyingchun <tianyingchun@outlook.com>
2024-06-11 14:41:30 +0800

    chore: migrate `pkg-up` to `package-up`

---
+++ .changeset/little-swans-tickle.md
@@ -0,0 +1,5 @@
+---
+"@hyperse/ts-node-paths": patch
+---
+
+migrate `pkg-up` to `package-up`
--- src/tool/searchMonoProjectDir.ts
+++ src/tool/searchMonoProjectDir.ts
@@ -1,13 +1,13 @@
 import { dirname } from 'node:path';
-import type { Options } from 'pkg-up';
-import { pkgUpSync } from 'pkg-up';
+import type { Options } from 'package-up';
+import { packageUpSync } from 'package-up';
 
 /**
  * Find the closest package.json file
  * @returns Returns the file path, or undefined if it could not be found.
  */
 export const searchMonoProjectDir = (options: Options) => {
-  const packageFile = pkgUpSync(options);
+  const packageFile = packageUpSync(options);
   if (packageFile && !/node_modules/.test(packageFile)) {
     return dirname(packageFile);
   }
